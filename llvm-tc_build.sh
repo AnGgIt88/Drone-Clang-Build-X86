3dd4db6fbd169ca95fe4a192c986ed11739d9bf5
NFS-Support <jarbull87@gmail.com>
2021-08-20 19:07:29 +0700

    Update llvm-tc_build.sh

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-# Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc
+
+git clone https://gitlab.kitware.com/cmake/cmake $(pwd)/cmake
+cd $(pwd)/cmake
+chmod +x bootstrap && ./bootstrap && make -j$(nproc --all) && make install
+git clone https://github.com/AnGgIt86/llvmTC $(pwd)/llvmTC -b llvm-tc
 cd $(pwd)/llvmTC
 bash build-tc.sh
